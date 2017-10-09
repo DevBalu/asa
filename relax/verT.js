b01c48a3ff4c4ad7b2e1049a0d23c6e5bc8232b7
DevBalu <DevBalu@yahoo.com>
2017-10-09 17:17:23 +0300

    in ICI implement effects after scroll, little change in contant of site and lit change in adapting of site for phone

--- relax/verT.js
+++ relax/verT.js
@@ -5,12 +5,13 @@ class Point{
 	constructor(){
 		this.x = Math.floor(Math.random() * c.width);
 		this.y = Math.floor(Math.random() * c.height);
-		this.radius = Math.floor(Math.random() * 5);
+		// this.radius = Math.floor(Math.random() * 5);
+		this.radius = 5;
 		this.vector = {
 			'x': Math.floor(Math.random() * 3)-1,
 			'y': Math.floor(Math.random() * 3)-1
 		};
-		this.speed = Math.floor(Math.random() * 2)+ 1;
+		this.speed = Math.floor(Math.random() * 3)+ 0;
 	}
 }
 
@@ -24,52 +25,87 @@ function countPart(nr) {
 	return arrPart;
 }
 
-function updateX(point){
-	point.x = point.x + point.speed + point.vector.x;
-}
+function update(point){
+	if(!(point.x >= 0)){
+		point.vector.x = 1;
+	}
 
-function updateY(point){
+	if(point.x > c.width || point.x == c.width){
+		point.vector.x = -1;
+	}
+
+	if(!(point.y >= 0)){
+		point.vector.y = 1;
+	}
+
+	if(point.y >= c.height){
+		point.vector.y = -1;
+	}
+
+	point.x = point.x + point.speed + point.vector.x;
 	point.y = point.y + point.speed + point.vector.y;
 }
 
 function show(point){
+	// var stoped = point.speed + point.vector.x;
+	// if (stoped <= 0) {
+	// 	point.vector.x = Math.floor(Math.random() * 3)-1;
+	// }
+
 	ctx.beginPath();
 	ctx.arc(point.x, point.y, point.radius,0,2*Math.PI);
 	ctx.fill();
 }
 
-function drawPart(nr){
-	// gen arr with particles
-	var arr = countPart(nr);
-	console.log(arr);
-
-	setInterval(function(){
-		ctx.clearRect(0, 0, c.width, c.height);
-		for(var i = 0; i < arr.length; i++){
-
-			if(arr[i].x == c.width || arr[i].x > c.width){
-				arr[i].vector.x = -1;
-				updateX(arr[i]);
-			}else if(arr[i].x == 0 || arr[i].x < c.width){
-				arr[i].vector.x = 1;
-				updateX(arr[i]);
-			}else {
-				updateX(arr[i]);
+/*** TEST */
+	function motion(nr){
+		var arr = countPart(nr);
+		setInterval(function(){
+			ctx.clearRect(0, 0, c.width, c.height);
+			for(let i = 0; i < arr.length; i++){
+				console.log(arr[i].vector.x);
+				update(arr[i])
+				show(arr[i]);
 			}
 
-			if(arr[i].y == c.height || arr[i].y > c.height){
-				arr[i].vector.y = -1;
-				updateY(arr[i]);
-			}else if(arr[i].y == 0 || arr[i].y < 0){
-				arr[i].vector.y = 1;
-				updateY(arr[i]);
-			}else{
-				updateY(arr[i]);
-			}
-			show(arr[i]);
-		}
+		}, .5)
+	}
+	motion(1);
 
-	}, .5)
-}
+/**/
 
-drawPart(50);
+// function drawPart(nr){
+// 	// gen arr with particles
+// 	var arr = countPart(nr);
+// 	console.log(arr);
+
+// 	setInterval(function(){
+// 		ctx.clearRect(0, 0, c.width, c.height);
+// 		for(var i = 0; i < arr.length; i++){
+
+// 			if(arr[i].x == c.width || arr[i].x > c.width){
+// 				arr[i].vector.x = -1;
+// 				updateX(arr[i]);
+// 			}else if(arr[i].x == 0 || arr[i].x < c.width){
+// 				arr[i].vector.x = 1;
+// 				updateX(arr[i]);
+// 			}else {
+// 				updateX(arr[i]);
+// 			}
+
+// 			if(arr[i].y == c.height || arr[i].y > c.height){
+// 				arr[i].vector.y = -1;
+// 				updateY(arr[i]);
+// 			}else if(arr[i].y == 0 || arr[i].y < 0){
+// 				arr[i].vector.y = 1;
+// 				updateY(arr[i]);
+// 			}else{
+// 				updateY(arr[i]);
+// 			}
+// 			show(arr[i]);
+// 		}
+
+// 	}, .5)
+// }
+
+// drawPart(50);
